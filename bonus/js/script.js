38fb334caf47f24c871f19cde8c11097ba422432
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-07-04 15:38:06 +0200

    bonus

---
+++ bonus/js/script.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+  el: "#root",
+  data: {
+    emailArray: [],
+  },
+  methods: {},
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          this.emailArray.push(response.data.response);
+        });
+    }
+  },
+});
